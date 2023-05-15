17e72566c53036efba1c2d57eb1df5770c2ae7fa
giovanastefan <89399240+giovanastefan@users.noreply.github.com>
2023-05-14 21:35:55 -0300

    Add get to users

--- backend/src/main/java/com/discoverCity/backend/controller/UserController.java
+++ backend/src/main/java/com/discoverCity/backend/controller/UserController.java
@@ -3,9 +3,12 @@ package com.discoverCity.backend.controller;
 import com.discoverCity.backend.model.User;
 import com.discoverCity.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -15,5 +18,10 @@ public class UserController {
     @PostMapping("/usuario")
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/usuarios")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
